8c55a4ba7e05c208d67f7a0135a4e3f9d744ae46
Your Name <you@example.com>
2024-11-28 10:14:54 +0530

    add(28/11/24)

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import WorkTop from './work/WorkTop';
+import WorkSecond from './work/WorkSecond';
 
 const Work = () => {
   return (
     <div>
       <WorkTop/>
+      <WorkSecond/>
     </div>
   );
 }
---
+++ src/components/work/WorkSecond.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../asset/styles/work/worksecond.css";
+
+const WorkSecond = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default WorkSecond;
