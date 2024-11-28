295276afe1f0be455acf4d29e5e37472edcbd0f8
Your Name <you@example.com>
2024-11-28 19:10:07 +0530

    add(28/11/24)

--- src/components/about/AboutTop.js
+++ src/components/about/AboutTop.js
@@ -3,8 +3,30 @@ import "../../asset/styles/about/abouttop.css";
 
 const AboutTop = () => {
   return (
-    <div>
-      
+    <div className="abouttop-container">
+      {/* Left Section */}
+      <div className="abouttop-left">
+        <h1>WE TURN<br />CULTURAL<br />VALUE</h1>
+      </div>
+
+      {/* Right Section */}
+      <div className="abouttop-right">
+        <h1>● INTO<br />COMPANY<br />VALUE</h1>
+        <h2>
+          BASIC/DEPT® is a global agency that<br/> 
+          thrives at the intersection of design, <br/>
+          data, and technology. Together, we’re<br/>
+           focused on transforming brands and<br/>
+            culture — across the world.
+        </h2>
+      </div>
+
+      {/* Horizontal Line Section */}
+      <div className="abouttop-line">
+        <span className="line-left">2010-</span>
+        <span className="line-center">PRESENT</span>
+        <span className="line-dot">●</span>
+      </div>
     </div>
   );
 }
