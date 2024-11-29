82347fef0d2a394a66107dc5229a3e02b48d2b63
Your Name <you@example.com>
2024-11-29 18:35:03 +0530

    add(29/11/24)

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NewsTop from './news/NewsTop';
+import HomeSix from './home/HomeSix';
 
 const News = () => {
   return (
     <div>
       <NewsTop/>
+      <HomeSix/>
     </div>
   );
 }
