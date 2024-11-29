e3f946c75211c63861c5fc3fea0c1ede1aabe9a4
Your Name <you@example.com>
2024-11-29 11:58:45 +0530

    add(29/11/24)

--- src/components/about/AboutSecond.js
+++ src/components/about/AboutSecond.js
@@ -3,9 +3,19 @@ import "../../asset/styles/about/aboutsecond.css";
 
 const AboutSecond = () => {
   return (
-    <div>
-      
+    <div className="aboutsecond-container">
+    {/* ऊपर की लाइन */}
+    <div className="line-container">
+      <hr className="horizontal-line" />
     </div>
+    {/* H1 टेक्स्ट */}
+    <h1 className="main-text">BASIC/DEPT ®</h1>
+    {/* नीचे की लाइन */}
+    <div className="line-container">
+     
+      <hr className="horizontal-line second-line" />
+    </div>
+  </div>
   );
 }
 
